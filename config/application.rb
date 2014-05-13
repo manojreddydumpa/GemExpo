d7a85278fd9cdd2b8c36a2cffef875f886f3a003
MANOJ <bigc@bigc.(none)>
2014-05-13 10:44:09 +0530

    Test commit 6

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ Bundler.require(*Rails.groups)
 
 module GemExpo
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = true
+    # config.assets.initialize_on_precompile = true
   end
 end
